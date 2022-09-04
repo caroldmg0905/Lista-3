2b7ad99ed512abb7a4e4555fd59ce4ebc5fad386
caroldmg0905 <99288502+caroldmg0905@users.noreply.github.com>
2022-09-04 10:32:46 -0300

    Add files via upload

---
+++ PrincipalEmp.java
@@ -0,0 +1,30 @@
+package listasDeExercicios.lista3.ex08;
+
+import java.util.Scanner;
+
+public class PrincipalEmp {
+    public static void main(String[] args) {
+        Empregado e = new Empregado();
+        Scanner entrada = new Scanner(System.in);
+
+        System.out.print("Nome: ");
+        String nome = entrada.nextLine();
+        e.setNome(nome);
+
+        System.out.print("Salário bruto: ");
+        double salarioBruto = entrada.nextDouble();
+        e.setSalarioBruto(salarioBruto);
+
+        System.out.print("Imposto: ");
+        double imposto = entrada.nextDouble();
+        e.setImposto(imposto);
+
+        e.buscasSalarioLiquido();
+
+        System.out.println("Qual a porcentagem para aumentar o salario? ");
+        double porcentagem = entrada.nextDouble();
+        e.aumentarSalario(porcentagem);
+        entrada.close();
+    }
+}
+
---
+++ Senha.java
@@ -0,0 +1,23 @@
+package listasDeExercicios.lista3.ex06;
+
+import java.util.Scanner;
+    
+public class Senha {
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+    
+            System.out.print("Digite a senha: ");
+            int senhaUser = entrada.nextInt();
+            int senha = 2002;
+    
+            while (senhaUser != senha) {
+                System.out.print("Senha Inválida! Tente Novamente: ");
+                senhaUser = entrada.nextInt();
+            }
+            if (senhaUser == senha) {
+                System.out.print("Acesso permitido!");
+            }
+            entrada.close();
+        }
+    } 
+
