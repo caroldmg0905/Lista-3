38076b10f229ea7fbbffa2a5777c178257b43e1d
caroldmg0905 <99288502+caroldmg0905@users.noreply.github.com>
2022-09-04 10:14:51 -0300

    Add files via upload

---
+++ Lojinha.java
@@ -0,0 +1,29 @@
+package listasDeExercicios.lista3.ex01.ex02;
+
+import java.util.Scanner;
+
+public class Lojinha {
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+
+        System.out.println("Preço unitário do produto: ");
+        float preco = entrada.nextFloat();
+
+        System.out.println("Quantidade comprada: ");
+        float qt = entrada.nextFloat();
+
+        System.out.println("Dinheiro recebido: ");
+        float dinheiro = entrada.nextFloat();
+
+        Lojinha.troco(preco, qt, dinheiro);
+
+        entrada.close();
+    }
+
+    public static void troco(float preco, float qt, float dinheiro) {
+        float total = qt * preco;
+        float troco = dinheiro - total;
+        System.out.println("TROCO = " + troco);
+    }
+}
+
---
+++ Pagamento.java
@@ -0,0 +1,26 @@
+package listasDeExercicios.lista3.ex01;
+
+import java.util.Scanner;
+
+public class Pagamento {
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+
+        Funcionario f = new Funcionario();
+
+        System.out.println("Nome: ");
+        String nome = entrada.nextLine();
+        f.setNome(nome);
+
+        System.out.println("Valor por hora: ");
+        float valor = entrada.nextFloat();
+        f.setValor(valor);
+
+        System.out.println("Horas trabalhadas: ");
+        int horasTrab = entrada.nextInt();
+        f.pagamento(horasTrab);
+
+        entrada.close();
+    }
+}
+
