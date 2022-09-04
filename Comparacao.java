caff9564e1880b4455bc529874b39c5ca2c43efe
caroldmg0905 <99288502+caroldmg0905@users.noreply.github.com>
2022-09-04 10:25:10 -0300

    Add files via upload

---
+++ Comparacao.java
@@ -0,0 +1,33 @@
+package listasDeExercicios.lista3.ex04;
+
+import java.util.Scanner;
+
+public class Comparacao {
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+
+        boolean funcionando = true;
+
+        do {
+            System.out.println("Digite dois números: ");
+            int x = entrada.nextInt();
+            int y = entrada.nextInt();
+
+            String sentido = "";
+
+            if (x > y) {
+                sentido = "DECRESCENTE!";
+            } else if (x < y) {
+                sentido = "CRESCENTE!";
+            } else if (x == y) {
+                sentido = "IGUAIS!";
+            }
+
+            System.out.println(sentido);
+        } while (funcionando);
+
+        entrada.close();
+    }
+}
+    
+
