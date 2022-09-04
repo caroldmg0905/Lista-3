270d949dfe62fa9936bd199bd05d78a51520610e
caroldmg0905 <99288502+caroldmg0905@users.noreply.github.com>
2022-09-04 10:24:23 -0300

    Add files via upload

---
+++ Calculadora.java
@@ -0,0 +1,30 @@
+package listasDeExercicios.lista3.ex05;
+
+public class Calculadora {
+    
+    private int valor;
+
+    public Calculadora() {
+    }
+
+    public Calculadora(int valor) {
+        this.valor = valor;
+    }
+
+    public void tabuada() {
+        for (int i = 1; i <= 10; i++) {
+            int resultado = this.valor * i;
+
+            System.out.println(this.valor + " x " + i + " = " + resultado);
+        }
+    }
+
+    public int getValor() {
+        return valor;
+    }
+
+    public void setValor(int valor) {
+        this.valor = valor;
+    }
+
+}
