41557d8defc9a12b6d05d4218b50e5e442ff79c9
caroldmg0905 <99288502+caroldmg0905@users.noreply.github.com>
2022-09-04 10:38:26 -0300

    Add files via upload

---
+++ Cachorro.java
@@ -0,0 +1,17 @@
+package listasDeExercicios.lista3.ex10;
+
+public class Cachorro extends Animal {
+
+    public Cachorro() {
+    }
+
+    public Cachorro(String nome) {
+        super(nome);
+    }
+
+    public String late() {
+        String som = "Auau";
+        return som;
+    }
+
+}
---
+++ Gato.java
@@ -0,0 +1,18 @@
+package listasDeExercicios.lista3.ex10;
+
+public class Gato extends Animal {
+
+    public Gato() {
+    }
+
+    public Gato(String nome) {
+        super(nome);
+    }
+
+    public String mia() {
+        String som = "Miauuu";
+
+        return som;
+    }
+
+}
